cb576e685caae56368667523f72f6b0cf2715c0e
Luiz <lf.fonseca.0808@gmail.com>
2025-09-26 19:25:31 -0300

    Corrigido V2

--- base_conhecimento.pl
+++ base_conhecimento.pl
@@ -22,7 +22,7 @@ trilha(redes_e_infraestrutura, 'Planejamento, administracao e monitoramento de r
 % ---------------------------------------------------
 
 % Inteligencia Artificial
-perfil(inteligencia_artificial, matematica_estatistica, 5).
+perfil(inteligencia_artificial, estatistica_avancada,  5).
 perfil(inteligencia_artificial, logica_programacao,    4).
 perfil(inteligencia_artificial, python,                2).
 perfil(inteligencia_artificial, machine_learning,      3).
@@ -32,7 +32,7 @@ perfil(inteligencia_artificial, deep_learning,         1).
 perfil(ciencia_de_dados, analise_dados,         5).
 perfil(ciencia_de_dados, sql_bancos,            4).
 perfil(ciencia_de_dados, frameworks_de_python,  3).
-perfil(ciencia_de_dados, matematica_estatistica,2).
+perfil(ciencia_de_dados, estatistica_basica,    2).
 perfil(ciencia_de_dados, gestao_projetos,       1).
 
 % Desenvolvimento Web
@@ -45,7 +45,7 @@ perfil(desenvolvimento_web, design_visual,  1).
 % Seguranca da Informacao
 perfil(seguranca_da_informacao, seguranca_defensiva, 5).
 perfil(seguranca_da_informacao, criptografia,        4).
-perfil(seguranca_da_informacao, redes_sistemas,      3).
+perfil(seguranca_da_informacao, segurana_redes_sistemas,      3).
 perfil(seguranca_da_informacao, sistemas_operacionais, 2).
 perfil(seguranca_da_informacao, testes_qualidade,    1).
 
@@ -62,7 +62,7 @@ perfil(redes_e_infraestrutura, sistemas_distribuidos, 1).
 % --------------------------
 
 % Inteligencia Artificial
-pergunta(1,  'Voce tem afinidade com matematica e estatistica?',            matematica_estatistica).
+pergunta(1,  'Voce tem afinidade com matematica e estatistica avançadas?',            estatistica_avancada).
 pergunta(2,  'Voce gosta de resolver problemas logicos de programacao?',    logica_programacao).
 pergunta(3,  'Voce programa (ou quer programar) frequentemente em Python?', python).
 pergunta(4,  'Voce se interessa por aprendizado de maquina/IA?',            machine_learning).
@@ -72,7 +72,7 @@ pergunta(5,  'Voce tem interesse em saber mais sobre modelos profundos de Machin
 pergunta(6,  'Voce tem interesse em analisar dados e extrair insights?',    analise_dados).
 pergunta(7,  'Voce gosta de trabalhar com bancos de dados e SQL?',          sql_bancos).
 pergunta(8,  'Voce gostaria de utilizar/aprender python e seus frameworks para analise de dados?', frameworks_de_python).
-pergunta(9,  'Voce tem facilidade com matematica para analise estatistica?', matematica_estatistica).
+pergunta(9,  'Voce tem interesse em aprender os fundamentos da estatistica?', estatistica_basica).
 pergunta(10, 'Voce gosta de planejar tarefas e prazos (gestao de projetos)?', gestao_projetos).
 
 % Desenvolvimento Web
@@ -85,7 +85,7 @@ pergunta(15, 'Voce curte design visual/estetica?',                          desi
 % Seguranca da Informacao
 pergunta(16, 'Voce se interessa por seguranca defensiva e protecao de dados?', seguranca_defensiva).
 pergunta(17, 'Criptografia e controles de acesso chamam sua atencao?',      criptografia).
-pergunta(18, 'Voce tem interesse em redes, protocolos e administracao de SO?', redes_sistemas).
+pergunta(18, 'Voce tem interesse em segurança de redes, protocolos e administracao de SO?', seguranca_redes_sistemas).
 pergunta(19, 'Voce gosta de entender sistemas operacionais e sua seguranca?', sistemas_operacionais).
 pergunta(20, 'Voce se preocupa com testes, qualidade e manutencao?',        testes_qualidade).
 
